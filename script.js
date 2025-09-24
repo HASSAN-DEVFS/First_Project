35161a92d5c2851030b29262ee53cf88a5ae847b
CYBER_TECH <hdjdjdjxjndj@gmail.com>
2025-09-24 10:32:30 +0100

    Ajoutation de task2

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ function factorielle(n) {
 }
 
 console.log(factorielle(num));
+
+
+
+var num = 123542;
+
+function compterChiffres(n) {
+  let compteur = 0;
+  let nombre = Math.abs(n); 
+  if (nombre === 0) return 1; 
+  
+  while (nombre > 0) {
+    nombre = Math.floor(nombre / 10); 
+    compteur++;
+  }
+  return compteur;
+}
+
+console.log(compterChiffres(num)); 
